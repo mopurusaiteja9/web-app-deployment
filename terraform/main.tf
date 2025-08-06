5c5a3bc7d99adcd890f6b355ed2320adf31ef14f
mopurusaiteja9 <164139536+mopurusaiteja9@users.noreply.github.com>
2025-08-06 10:20:16 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   name   = "webapp-vpc"
   cidr   = "10.0.0.0/16"
-  azs             = ["us-east-1a", "us-east-1b"]
+  azs             = ["ap-south-1a", "ap-south-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
   enable_nat_gateway = true
