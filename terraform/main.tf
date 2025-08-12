e103ff8d3f9d0c6de82d394dcf2d8ae113a6b86e
mopurusaiteja9 <164139536+mopurusaiteja9@users.noreply.github.com>
2025-08-12 08:40:45 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ module "vpc" {
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "webapp-cluster"
+  cluster_name    = var.cluster_name
   cluster_version = "1.27"
   subnets         = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
